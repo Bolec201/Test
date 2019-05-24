6b4f235588a4da21ce4cd0246dade1892509e558
AleksanderBolec <aleksanderw2001@gmail.com>
2019-05-24 14:40:41 +0200

    ćwiczenie 10 ze strony 66

---
+++ Project2/project 2 l.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string tekst;
+	int liczba_sp = 0;
+
+	cout << "Podaj tekst: ";
+	getline(cin, tekst);
+
+	for (int i = 0; i < tekst.length(); i++)
+	{
+		if (tekst[i] == ' ')
+			liczba_sp++;
+	}
+
+	cout << "Ilosc spacji: " << liczba_sp;
+
+	system("pause");
+}
+
